e75638020a8b743e86c03dd7e1c4fc70a6889103
singhvaishu <vaishali231305@gmail.com>
2024-05-18 09:18:21 +0530

    api reset

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { userRoute } from './routes/userRoute.js';
+import { residencyRoute } from './routes/residencyRoute.js';
+
+dotenv.config()
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.use('/api/user', userRoute)
+app.use("/api/residency", residencyRoute)
+
+app.get("/home", (req, res) => {
+    res.send("this is home page");
+  });
